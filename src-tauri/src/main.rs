9165278c069b28c55201fb531e7ac6c80cd2a2e7
root309 <choco00101x@gmail.com>
2023-11-09 20:38:31 +0900

    tauri test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,18 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri::Manager;
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            let _ = window.set_position(tauri::Position::Physical(tauri::PhysicalPosition { x: 0, y: 0 }));
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![get_system_info])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
+#[tauri::command]
+fn get_system_info() -> String {
+    "Dummy System Info".to_string() // temp
+}
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { invoke } from '@tauri-apps/api';
 
 function App() {
+  const [systemInfo, setSystemInfo] = useState('');
+
+  useEffect(() => {
+    async function fetchSystemInfo() {
+      try {
+        const info = await invoke('get_system_info');
+        setSystemInfo(info);
+      } catch (e) {
+        console.error('Failed to fetch system info', e);
+      }
+    }
+
+    fetchSystemInfo();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>System Info: {systemInfo}</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
