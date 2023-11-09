1b6e9053aafba3912e5d63461a7174ac9c255f05
root309 <choco00101x@gmail.com>
2023-11-09 21:53:13 +0900

    feat: draw cpu usage, memory usage, time

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,34 @@
-use tauri::Manager;
+use sysinfo::{System, SystemExt, CpuExt};
+use serde_json::json;
+use chrono::{DateTime, Utc, FixedOffset};
+
+#[tauri::command]
+fn get_system_info() -> String {
+    let mut system = System::new_all();
+    system.refresh_all();
+
+    let cpu_usage = system.global_cpu_info().cpu_usage();
+    let memory_usage = (system.used_memory() as f64 / system.total_memory() as f64) * 100.0;
+
+    let offset = FixedOffset::east(9 * 3600);
+    // UTC
+    let time: DateTime<Utc> = Utc::now();
+    let local_time = time.with_timezone(&offset);
+
+    let formatted_time = local_time.format("%H:%M").to_string();
+
+    json!({
+        "cpu_usage": cpu_usage,
+        "memory_usage": memory_usage,
+        "time": formatted_time,
+    })
+    .to_string()
+}
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let window = app.get_window("main").unwrap();
-            let _ = window.set_position(tauri::Position::Physical(tauri::PhysicalPosition { x: 0, y: 0 }));
-            Ok(())
-        })
         .invoke_handler(tauri::generate_handler![get_system_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-#[tauri::command]
-fn get_system_info() -> String {
-    "Dummy System Info".to_string() // temp
 }
 
--- src/App.js
+++ src/App.js
@@ -2,26 +2,54 @@ import React, { useState, useEffect } from 'react';
 import { invoke } from '@tauri-apps/api';
 
 function App() {
-  const [systemInfo, setSystemInfo] = useState('');
+  const [systemInfo, setSystemInfo] = useState({ cpu_usage: 0, memory_usage: 0, time: '' });
 
   useEffect(() => {
     async function fetchSystemInfo() {
       try {
         const info = await invoke('get_system_info');
-        setSystemInfo(info);
+        setSystemInfo(JSON.parse(info));
       } catch (e) {
         console.error('Failed to fetch system info', e);
       }
     }
 
-    fetchSystemInfo();
+    const intervalId = setInterval(fetchSystemInfo, 1000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
+  // ステータスバースタイル
+  const statusBarStyle = {
+    display: 'flex',
+    justifyContent: 'start',
+    alignItems: 'center',
+    width: '100%', // 全幅
+    height: '25px', // 高さ
+    backgroundColor: '#1b1b1b', // 背景色
+    color: '#ffffff', // テキストの色
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    padding: '5px 10px',
+    boxSizing: 'border-box',
+    fontFamily: '"Roboto Mono", monospace', // フォント
+    fontSize: '12px', // フォントサイズ
+    zIndex: 9999, // 他の要素の上に表示
+  };
+
+  // 各情報表示用のスタイル
+  const infoStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '20px', // 右側の余白
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>System Info: {systemInfo}</p>
-      </header>
+    <div style={statusBarStyle}>
+      <div style={infoStyle}>CPU: {systemInfo.cpu_usage.toFixed(2)}%</div>
+      <div style={infoStyle}>RAM: {systemInfo.memory_usage.toFixed(2)}%</div>
+      <div style={infoStyle}>Time: {systemInfo.time}</div>
     </div>
   );
 }
